0c5cd14ff085b6d2aade5e6a92d3075cc08774d6
AaronC714 <aaronc0714@gmail.com>
2025-03-12 21:07:21 +0800

    process and results added

--- batch_2/input_2_numbers_print_smaller_number.py
+++ batch_2/input_2_numbers_print_smaller_number.py
@@ -5,3 +5,10 @@
 
 num1 = int(input("Enter a number: "))
 num2 = int(input("Enter a number: "))
+
+if num1 > num2:
+    print (num2)
+elif num2 > num1:
+    print (num1)
+else:
+    print ("Numbers are equal")
