5324b69e0eeb50107bd609e067b657cc34dd43d0
AaronC714 <aaronc0714@gmail.com>
2025-03-12 23:45:30 +0800

    pseudocode added

--- batch_2/input_2_numbers_print_all_numbers_between_two_numbers.py
+++ batch_2/input_2_numbers_print_all_numbers_between_two_numbers.py
@@ -1,1 +1,4 @@
 #Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
+# input two numbers
+# process
+# print all the numbers between the two numbers
