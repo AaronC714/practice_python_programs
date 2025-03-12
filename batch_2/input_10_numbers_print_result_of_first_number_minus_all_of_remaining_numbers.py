cbea55d24b0bc5e2cd267cd5d92c13d2605c3b9f
AaronC714 <aaronc0714@gmail.com>
2025-03-12 22:43:45 +0800

    process and results added

--- batch_2/input_10_numbers_print_result_of_first_number_minus_all_of_remaining_numbers.py
+++ batch_2/input_10_numbers_print_result_of_first_number_minus_all_of_remaining_numbers.py
@@ -5,3 +5,11 @@
 
 
 numbers = [int(input(f"Enter number {i+1}: ")) for i in range(10)]
+
+first_number = numbers[0]
+
+result = first_number
+for num in numbers[1:]:
+    result -= num
+
+print("The result of the first number minus all remaining numbers is:", result)
