eed40df5411dfdedd511688aa9094098179999f3
AaronC714 <aaronc0714@gmail.com>
2025-03-12 21:23:35 +0800

    updated pseudocode and results added

--- batch_2/input_2_numbers_print_difference_of_two_numbers.py
+++ batch_2/input_2_numbers_print_difference_of_two_numbers.py
@@ -1,7 +1,9 @@
 #Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
 # input two numbers
 # process
-# print sum of the two numbers
+# print difference of the two numbers
 
 num1 = int(input("Enter a number: "))
 num2 = int(input("Enter a number: "))
+
+print("The sum of both numbers is: ", num1 - num2)
