04f51432e98a12420dae41f4debfc8114b45b67d
AaronC714 <aaronc0714@gmail.com>
2025-03-12 18:14:19 +0800

    process and output added

--- batch_1/input_2_numbers_print_quotient_of_two_numbers_with_the_decimal_point.py
+++ batch_1/input_2_numbers_print_quotient_of_two_numbers_with_the_decimal_point.py
@@ -5,3 +5,8 @@
 
 num1 = int(input("Enter a number: "))
 num2 = int(input("Enter a number: "))
+
+if num1 == 0 or num2 == 0:
+    print ("zero cannot be an input")
+else:
+    print("the quotient is: ", num1 / num2)
