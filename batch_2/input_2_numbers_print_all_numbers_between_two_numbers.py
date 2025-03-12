d17702ee58d041e4151d8827841078d5349c1099
AaronC714 <aaronc0714@gmail.com>
2025-03-12 23:51:50 +0800

    process and numbers between inputs added

--- batch_2/input_2_numbers_print_all_numbers_between_two_numbers.py
+++ batch_2/input_2_numbers_print_all_numbers_between_two_numbers.py
@@ -5,3 +5,10 @@
 
 num1 = int(input("Enter a number: "))
 num2 = int(input("Enter a number: "))
+
+if num1 > num2:
+    num1, num2 = num2, num1  # numbers should be in increasing order
+
+print(f"Numbers between {num1} and {num2}:")
+for num in range(num1 + 1, num2):
+    print(num)
