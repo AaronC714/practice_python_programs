da71b32a9604c7863b23d391983df6942a10e460
AaronC714 <aaronc0714@gmail.com>
2025-03-12 19:35:32 +0800

    process and result added

--- batch_1/input_10_numbers_print_how_many_are_odd_numbers.py
+++ batch_1/input_10_numbers_print_how_many_are_odd_numbers.py
@@ -4,3 +4,6 @@
 # print number of odd numbers 
 
 num1 = [int(input("Type a number: ")) for _ in range (10)]
+odd_count = sum(1 for num in num1 if num % 2 != 0)
+
+print("Count of odd numbers:", odd_count)
