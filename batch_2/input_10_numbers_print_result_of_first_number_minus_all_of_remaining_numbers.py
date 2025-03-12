ea79597b9203814e489afc380cd231c5509b329c
AaronC714 <aaronc0714@gmail.com>
2025-03-12 22:34:21 +0800

    pseudocode added

--- batch_2/input_10_numbers_print_result_of_first_number_minus_all_of_remaining_numbers.py
+++ batch_2/input_10_numbers_print_result_of_first_number_minus_all_of_remaining_numbers.py
@@ -1,1 +1,4 @@
 #Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
+#input 10 numbers
+#process
+#print the result of the first number minus all of the remaining numbers.
