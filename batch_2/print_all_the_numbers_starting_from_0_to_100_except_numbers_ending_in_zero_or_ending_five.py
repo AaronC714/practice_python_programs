2c4fa1cbad216bccb23c0053ece27649cb224278
AaronC714 <aaronc0714@gmail.com>
2025-03-12 23:30:52 +0800

    pseudocode added

--- batch_2/print_all_the_numbers_starting_from_0_to_100_except_numbers_ending_in_zero_or_ending_five.py
+++ batch_2/print_all_the_numbers_starting_from_0_to_100_except_numbers_ending_in_zero_or_ending_five.py
@@ -1,1 +1,4 @@
 #Prog09: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
+#number in range
+#process
+#print 0 to 100 except numbers ending in zero or ending five
