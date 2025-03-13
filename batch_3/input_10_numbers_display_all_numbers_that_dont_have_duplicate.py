23f29c42fa65a1b297815b5c99cc78d9401f157d
AaronC714 <aaronc0714@gmail.com>
2025-03-13 09:38:42 +0800

    Removed batch_3 directory and all its files

--- batch_3/input_10_numbers_display_all_numbers_that_dont_have_duplicate.py
+++
@@ -1,8 +0,0 @@
-#Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
-#input 10 numbers
-#process
-#display all numbers that don't have duplicate
-
-numbers = [int(input("type a number: ")) for _ in range (10)]
-no_duplicates = [num for num in numbers if numbers.count(num) == 1]
-print (no_duplicates)
