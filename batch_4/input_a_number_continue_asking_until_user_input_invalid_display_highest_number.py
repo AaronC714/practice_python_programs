d894ee224e670c9590472c89e145d1d935ed70b8
AaronC714 <aaronc0714@gmail.com>
2025-03-13 18:19:38 +0800

    ipo added

--- batch_4/input_a_number_continue_asking_until_user_input_invalid_display_highest_number.py
+++ batch_4/input_a_number_continue_asking_until_user_input_invalid_display_highest_number.py
@@ -1,1 +1,4 @@
-#
+#Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
+#ask input from user
+#process what number is the highest
+#print highest number
