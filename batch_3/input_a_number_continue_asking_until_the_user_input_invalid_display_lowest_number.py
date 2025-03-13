29c884d0520501e68eadfa218e28f9c883f97136
AaronC714 <aaronc0714@gmail.com>
2025-03-13 11:29:54 +0800

    declareing and user input added

--- batch_3/input_a_number_continue_asking_until_the_user_input_invalid_display_lowest_number.py
+++ batch_3/input_a_number_continue_asking_until_the_user_input_invalid_display_lowest_number.py
@@ -2,3 +2,11 @@
 #ask user for input (number)
 #continue asking (loop)
 #display lowest number
+
+# Initialize a variable to store the lowest number
+lowest_number = None
+
+while True:
+    try:
+        # Ask the user for a number
+        num = int(input("Enter a number (or any non-number to stop): "))
