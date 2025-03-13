781e3afbba9f6cbb9bcae6b947d32c6dfbe033c0
AaronC714 <aaronc0714@gmail.com>
2025-03-13 10:37:06 +0800

    user input of 10 numbers added

--- batch_3/input_a_number_until_user_input_is_invalid_display_onique_after_input_inputted_number_dont_have_duplicate_display_duplicate_after_input_when_inputted_number_have_duplicate.py
+++ batch_3/input_a_number_until_user_input_is_invalid_display_onique_after_input_inputted_number_dont_have_duplicate_display_duplicate_after_input_when_inputted_number_have_duplicate.py
@@ -3,3 +3,11 @@
 #continue asking until the user input is invalid
 #Display unique after input when the inputted number don't have duplicate
 #display duplicate after input when the inputted number have duplicate
+
+
+inputted_numbers = []
+
+while True:
+    try:
+        # Ask user for a number
+        num = int(input("Enter a number (or any non-number to stop): "))
