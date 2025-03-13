8ffd2f6c37e295b3ea3d131ba0e880791aa10e5d
AaronC714 <aaronc0714@gmail.com>
2025-03-13 11:50:58 +0800

    pseudocode added

--- batch_3/input_number_continue_asking_until_user_input_invalid_display_number_from_lowest_to_highest.py
+++ batch_3/input_number_continue_asking_until_user_input_invalid_display_number_from_lowest_to_highest.py
@@ -1,1 +1,4 @@
-#
+#Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from lowest to highest. Clue: sort() function
+#ask user for input
+#continue asking until invalid
+#display number in ascending order
