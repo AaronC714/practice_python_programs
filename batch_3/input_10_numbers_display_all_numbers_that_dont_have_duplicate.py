888af808f1ee835717bcdf25fc1e68df1c843507
AaronC714 <aaronc0714@gmail.com>
2025-03-13 08:56:59 +0800

    final result added

--- batch_3/input_10_numbers_display_all_numbers_that_dont_have_duplicate.py
+++ batch_3/input_10_numbers_display_all_numbers_that_dont_have_duplicate.py
@@ -4,4 +4,5 @@
 #display all numbers that don't have duplicate
 
 numbers = [int(input("type a number: ")) for _ in range (10)]
-unique_numbers = [num for num in numbers if numbers.count(num) == 1]
+no_duplicates = [num for num in numbers if numbers.count(num) == 1]
+print (no_duplicates)
