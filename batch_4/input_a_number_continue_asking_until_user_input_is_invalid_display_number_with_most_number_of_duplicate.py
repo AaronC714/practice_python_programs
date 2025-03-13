e425bed6651a52b39e88f55b944ff5611400c8a0
AaronC714 <aaronc0714@gmail.com>
2025-03-13 17:58:05 +0800

    updated ipo and added storing variable in user input

--- batch_4/input_a_number_continue_asking_until_user_input_is_invalid_display_number_with_most_number_of_duplicate.py
+++ batch_4/input_a_number_continue_asking_until_user_input_is_invalid_display_number_with_most_number_of_duplicate.py
@@ -1,6 +1,10 @@
 #Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
-#user input of 10 numbers
+#user input numbers
 #process
 #display the number with the most number of duplicate
 
-inputted_num = [int(input(f"Enter number {i+1}: ")) for i in range(10)]
+inputted_numbers = []
+
+while True:
+    try:
+        num = int(input("Enter a number (or any non-number to stop): "))
