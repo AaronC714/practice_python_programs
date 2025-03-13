92e60aa1e6f641787bf930ab64ab1f8b36e68821
AaronC714 <aaronc0714@gmail.com>
2025-03-13 19:05:19 +0800

    ipo added

--- batch_4/input_a_number_continue_asking_until_user_input_invalid_display_average.py
+++ batch_4/input_a_number_continue_asking_until_user_input_invalid_display_average.py
@@ -1,1 +1,4 @@
 #Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
+#ask input from user
+#continue asking until non integer input
+#compute average
