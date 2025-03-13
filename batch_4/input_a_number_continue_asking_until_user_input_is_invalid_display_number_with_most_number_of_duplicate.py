87848eca3bcb36a7f5c762ceaae88428331fe995
AaronC714 <aaronc0714@gmail.com>
2025-03-13 17:55:38 +0800

    ipo added

--- batch_4/input_a_number_continue_asking_until_user_input_is_invalid_display_number_with_most_number_of_duplicate.py
+++ batch_4/input_a_number_continue_asking_until_user_input_is_invalid_display_number_with_most_number_of_duplicate.py
@@ -1,1 +1,4 @@
-#
+#Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
+#user input of 10 numbers
+#process
+#display the number with the most number of duplicate
