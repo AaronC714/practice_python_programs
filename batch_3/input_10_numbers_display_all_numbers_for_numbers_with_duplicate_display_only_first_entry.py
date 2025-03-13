25929e8a8598a346ddd383257e4a1b58f04649a1
AaronC714 <aaronc0714@gmail.com>
2025-03-13 10:03:50 +0800

    pseudocode added

--- batch_3/input_10_numbers_display_all_numbers_for_numbers_with_duplicate_display_only_first_entry.py
+++ batch_3/input_10_numbers_display_all_numbers_for_numbers_with_duplicate_display_only_first_entry.py
@@ -1,1 +1,4 @@
-#
+#Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
+#input 10 numbers
+#process
+#display all numbers. For numbers with duplicate, display only the first entry.
