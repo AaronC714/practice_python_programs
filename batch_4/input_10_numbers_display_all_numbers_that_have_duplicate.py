ea6cdcae509f84c0375631eacb5a97944a741c21
AaronC714 <aaronc0714@gmail.com>
2025-03-13 17:39:34 +0800

    user input added

--- batch_4/input_10_numbers_display_all_numbers_that_have_duplicate.py
+++ batch_4/input_10_numbers_display_all_numbers_that_have_duplicate.py
@@ -1,3 +1,5 @@
 #Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
 #user input of 10 numbers 
 #display all numbers that have duplicate
+
+inputted_num = [int(input(f"Enter number {i+1}: ")) for i in range(10)]
