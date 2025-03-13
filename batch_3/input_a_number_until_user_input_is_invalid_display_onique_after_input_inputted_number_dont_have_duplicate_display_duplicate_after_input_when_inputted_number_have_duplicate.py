32100f9c668fb09ff25779d08f2e3e9403537fca
AaronC714 <aaronc0714@gmail.com>
2025-03-13 10:32:49 +0800

    pseudocode added

--- batch_3/input_a_number_until_user_input_is_invalid_display_onique_after_input_inputted_number_dont_have_duplicate_display_duplicate_after_input_when_inputted_number_have_duplicate.py
+++ batch_3/input_a_number_until_user_input_is_invalid_display_onique_after_input_inputted_number_dont_have_duplicate_display_duplicate_after_input_when_inputted_number_have_duplicate.py
@@ -1,1 +1,5 @@
-#
+#Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
+#user input
+#continue asking until the user input is invalid
+#Display unique after input when the inputted number don't have duplicate
+#display duplicate after input when the inputted number have duplicate
