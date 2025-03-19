83c8a921f72f864b702b53f8cb39c46d9dc48db2
AaronC714 <aaronc0714@gmail.com>
2025-03-19 16:59:01 +0800

    pseudocode added

--- batch_5/input_number_0_to_1000_print__number_in_6_digit_format_add_zeros_at_beginning_to_complete_6_digit.py
+++ batch_5/input_number_0_to_1000_print__number_in_6_digit_format_add_zeros_at_beginning_to_complete_6_digit.py
@@ -1,1 +1,4 @@
-#
+#Create a program that ask the user to input a number (0-1000). Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
+#ask number from user (0 to 1000)
+#process
+#Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
