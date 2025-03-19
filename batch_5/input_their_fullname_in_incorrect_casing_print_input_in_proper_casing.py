f8fcf077c8be24c3985d907e2bc47683113a209f
AaronC714 <aaronc0714@gmail.com>
2025-03-19 17:29:10 +0800

    pseudocode added

--- batch_5/input_their_fullname_in_incorrect_casing_print_input_in_proper_casing.py
+++ batch_5/input_their_fullname_in_incorrect_casing_print_input_in_proper_casing.py
@@ -1,1 +1,4 @@
 #Prog05: Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
+#ask user for name in incorrect casing
+#process in fixing casing
+#print full name in right casings
