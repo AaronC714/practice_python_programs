626d843553ff9d198ae47d157a77bfa1ccfa9bf4
AaronC714 <aaronc0714@gmail.com>
2025-03-19 17:59:24 +0800

    pseudocode added

--- batch_5/input_their_fullname_print_number_of_characters_in_input.py
+++ batch_5/input_their_fullname_print_number_of_characters_in_input.py
@@ -1,1 +1,4 @@
 #Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input.
+#ask user for full name
+#count number characters of name
+#print total number
