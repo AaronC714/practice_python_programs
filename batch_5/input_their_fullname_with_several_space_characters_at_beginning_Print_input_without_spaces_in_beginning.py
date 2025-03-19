1ba9c7fab62fba2c266af474274f2d2b3f8faff9
AaronC714 <aaronc0714@gmail.com>
2025-03-19 10:08:31 +0800

    pseudocode added

--- batch_5/input_their_fullname_with_several_space_characters_at_beginning_Print_input_without_spaces_in_beginning.py
+++ batch_5/input_their_fullname_with_several_space_characters_at_beginning_Print_input_without_spaces_in_beginning.py
@@ -1,1 +1,4 @@
 #Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
+#ask user for full name with with several space characters at the beginning
+#process
+#print full name without the spaces
