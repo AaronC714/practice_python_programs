78bb272f316f847e1c402b5f32aa95d8e7b12014
AaronC714 <aaronc0714@gmail.com>
2025-03-19 17:21:44 +0800

    pseudocode added

--- batch_5/input_their_fullname_print_the_input_all_lower_case.py
+++ batch_5/input_their_fullname_print_the_input_all_lower_case.py
@@ -1,1 +1,4 @@
 #Prog04: Create a program that ask the user to input their fullname. Print the input in all lower case.
+#ask users full name
+#process to full name
+#print user's name in lowercase letters
