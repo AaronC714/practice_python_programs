d1ce8a6414bde76f07c9023fb12b4c5c44bc223c
AaronC714 <aaronc0714@gmail.com>
2025-03-19 17:48:18 +0800

    pseudocode added

--- batch_5/input_complete_statement_print_number_of_words_in_input.py
+++ batch_5/input_complete_statement_print_number_of_words_in_input.py
@@ -1,1 +1,4 @@
-#Prog07: Create a program that ask the user to input a complete statement. Print the number of words in the input. 
+#Prog07: Create a program that ask the user to input a complete statement. Print the number of words in the input.
+#ask user for statement
+#count number of words
+#print number of words
