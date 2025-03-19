6ba70756448656b1dfa12b91f048aa4336a48591
AaronC714 <aaronc0714@gmail.com>
2025-03-19 17:38:48 +0800

    pseudocode added

--- batch_5/input_their_fullname_in_incorrect_casing_print_each_character_of_input_in_reverse_casing.py
+++ batch_5/input_their_fullname_in_incorrect_casing_print_each_character_of_input_in_reverse_casing.py
@@ -1,1 +1,4 @@
 #Prog06: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
+#ask user for their full name in incorrect casing
+#reverse casing
+#print each letter of the name in reverse casing
