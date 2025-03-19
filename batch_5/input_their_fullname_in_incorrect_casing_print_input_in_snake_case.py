21df6f457988b164af2cf57ea08cf776e5b074e5
AaronC714 <aaronc0714@gmail.com>
2025-03-19 18:21:09 +0800

    pseudocode added

--- batch_5/input_their_fullname_in_incorrect_casing_print_input_in_snake_case.py
+++ batch_5/input_their_fullname_in_incorrect_casing_print_input_in_snake_case.py
@@ -1,1 +1,4 @@
 #Prog10: Create a program that ask the user to input their fullname in incorrect casing. Print the input in snake case.
+#ask user for full name
+#process snake case
+#print name in snake case
