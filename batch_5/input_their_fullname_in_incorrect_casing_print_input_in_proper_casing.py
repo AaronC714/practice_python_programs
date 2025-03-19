35023f7beec2713f5f85207273fa1d5d24e6cbbd
AaronC714 <aaronc0714@gmail.com>
2025-03-19 17:31:41 +0800

    user input added

--- batch_5/input_their_fullname_in_incorrect_casing_print_input_in_proper_casing.py
+++ batch_5/input_their_fullname_in_incorrect_casing_print_input_in_proper_casing.py
@@ -2,3 +2,5 @@
 #ask user for name in incorrect casing
 #process in fixing casing
 #print full name in right casings
+
+user_name = str(input("Type your full name in an incorrect casing for (e.g.  jUAn DEla CrUZ): "))
