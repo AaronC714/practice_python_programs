a134554ae17dcc742b3927a9734211744397a300
AaronC714 <aaronc0714@gmail.com>
2025-03-19 18:09:16 +0800

    pseudocode added

--- batch_5/input_their_fullname_in_incorrect_casing_print_the_input_in_pascal_case.py
+++ batch_5/input_their_fullname_in_incorrect_casing_print_the_input_in_pascal_case.py
@@ -1,1 +1,4 @@
 #Prog09: Create a program that ask the user to input their fullname in incorrect casing. Print the input in pascal case.
+#ask name from user in incorrect casing
+#process pascal case
+#print name in pascal case
